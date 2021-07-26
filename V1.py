7ce27702674e15711c7df97da54cd51ace9c5ca7
Islam <islammnecy96@gmail.com>
2021-07-26 15:27:12 -0700

    Thanos code 1

--- V1.py
+++ V1.py
@@ -1,1 +1,8 @@
-print('hi')
+import os
+import random
+from random import sample
+
+g=os.listdir(r"C:\Users\MaRkZ\Desktop\AI\LEC 4\Universe")
+
+for i in sample(g,1):
+    os.remove(os.path.join(r"C:\U sers\MaRkZ\Desktop\AI\LEC 4\Universe", i))
